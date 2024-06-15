d017deedb5e0a808adccb97916f46f48d4878a77
Sumit Ojha <122687829+AndroidDeveloper98@users.noreply.github.com>
2024-06-15 13:13:52 +0530

    init

---
+++ lib/viewModel/splash_service.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:mvvm_setup/viewModel/user_view_model.dart';
+
+import '../utils/routes/routes_names.dart';
+
+class SplashService {
+  static void checkAuthentication(BuildContext context) async {
+    final userViewModel = UserViewModel();
+
+    final user = await userViewModel.getUser();
+
+    if (user!.token.toString() == "null" || user.token.toString() == "") {
+      await Future.delayed(const Duration(seconds: 3));
+      Navigator.pushNamed(context, RouteNames.login);
+    } else {
+      await Future.delayed(const Duration(seconds: 3));
+      Navigator.pushNamed(context, RouteNames.home);
+    }
+  }
+}
